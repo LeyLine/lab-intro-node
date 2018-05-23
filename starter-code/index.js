44d5b5e8d41251ddcbb2921f5da29777c588f792
Adrian Llerena <a.llerena001@gmail.com>
2018-05-22 20:52:44 -0400

    done with errors

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,30 +1,44 @@
-var SortedList = function () {
+var SortedList = function() {
   this.length = 0;
   this.items = [];
-
 };
 
-SortedList.prototype.add = function (item) {
+SortedList.prototype.add = function(item) {
   this.items.push(item);
-  this.items.sort(function (a, b) {
+  this.items.sort(function(a, b) {
     return a - b;
   });
   this.length++;
 };
-SortedList.prototype.get = function (pos) {
+SortedList.prototype.get = function(pos) {
   if (pos > this.items.length || pos < 1) {
-    return new Error('OutOfBounds');
+    return new Error("OutOfBounds");
   }
   return this.items[pos - 1];
 };
-SortedList.prototype.max = function () {
+SortedList.prototype.max = function() {
   return Math.max.apply(null, this.items);
 };
-SortedList.prototype.min = function () {
+SortedList.prototype.min = function() {
   return Math.min.apply(null, this.items);
-
 };
-SortedList.prototype.average = function () {};
-SortedList.prototype.sum = function () {};
+SortedList.prototype.average = function() {
+  var sum = this.items.reduce(function(a, b) {
+    return a + b;
+  });
+  var avg = sum / this.items.length;
+
+  if (this.items == null) {
+    return new Error("OutOfBounds");
+  } else {
+    return avg;
+  }
+};
+SortedList.prototype.sum = function() {
+  var sum = this.items.reduce(function(a, b) {
+    return a + b;
+  });
+  return sum;
+};
 
 module.exports = SortedList;
